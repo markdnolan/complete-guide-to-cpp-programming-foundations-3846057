a7f11f5052a78d41490becaace899f35b1581d1e
Mark Nolan <140343931+markdnolan@users.noreply.github.com>
2026-05-14 20:05:22 +0000

    added a comment

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,6 +1,7 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 00_03
-// Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
+// Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño
+// Test, I added a new comment 
 
 #include <iostream>
 
