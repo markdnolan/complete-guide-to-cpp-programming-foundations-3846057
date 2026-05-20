addf5918d2b5cb35e7dd2354b490cd0cf775a408
Mark Nolan <140343931+markdnolan@users.noreply.github.com>
2026-05-20 15:08:37 +0000

    Added comments

--- src/Ch01/01_09b/CodeDemo.cpp
+++ src/Ch01/01_09b/CodeDemo.cpp
@@ -1,16 +1,21 @@
 // Complete Guide to C++ Programming Foundations
 // Challenge 01_09
-// Terminal Interaction, by Eduardo Corpeño 
+// Terminal Interaction, by Eduardo Corpeño
+// Challenge completed by Mark Nolan 
 
 #include <iostream>
+// Include the standard library "string" function to use string variables
 #include <string>
 
 int main(){
+    // define a string variable called 'name'
     std::string name;
+    // std::flush ensures all ouput is sent to stdout before moving on
     std::cout << "Enter your name: " << std::flush;
     std::cin >> name;
     std::cout << "Nice to meet you " << name << "!" << std::endl;
-
+    // Two linefeeds to distinguish program messages from debugger messages when code is run
     std::cout << std::endl << std::endl;
+    // function type is 'int' so need to return an integer
     return 0;
 }
